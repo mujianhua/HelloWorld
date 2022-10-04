1cb71afd62ca5a5600a1f7a8ac4069f3c927de33
mujianhua <jianhua_mu@163.com>
2022-10-04 08:05:16 +0000

    add hash table

---
+++ leetcode/hast_table/hast_table.cc
@@ -0,0 +1,76 @@
+/**
+ * \brief 用来快速判断一个元素是否出现集合里.
+ **/
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    /********************** 202. 快乐数 *********************/
+    bool isHappy(int n)
+    {
+        unordered_set<int> record_set;
+        while (true)
+        {
+            int sum = GetSum(n);
+            if (sum == 1)
+            {
+                return true;
+            }
+            if (record_set.find(sum) != record_set.end())
+            {
+                return false;
+            }
+            else
+            {
+                record_set.insert(sum);
+            }
+            n = sum;
+        }
+    }
+    int GetSum(int n)
+    {
+        int sum = 0;
+        while (n)
+        {
+            sum += (n % 10) * (n % 10);
+            n = n / 10;
+        }
+        return sum;
+    }
+    /*****************************************************************/
+
+    /*****************************************************************/
+    // 349. 两个数组的交集
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_set<int> record_set;
+        unordered_set<int> ret;
+        for (int num : nums1)
+        {
+            if (record_set.find(num) == record_set.end())
+            {
+                record_set.insert(num);
+            }
+        }
+        for (int num : nums2)
+        {
+            // 发现和 record_set 中相同的元素
+            if (record_set.find(num) != record_set.end())
+            {
+                ret.insert(num);
+            }
+        }
+        return vector<int>(ret.begin(), ret.end());
+    }
+    /*****************************************************************/
+};
+
+int main()
+{
+    return 0;
+}
--- leetcode/linked_list/linked_list.cc
+++ leetcode/linked_list/linked_list.cc
@@ -17,63 +17,135 @@ class Solution {
     }
     return dummy_head->next;
   }
+
+  /* 206. 反转链表 */
+  ListNode* reverseList(ListNode* head) {
+    ListNode* pre = nullptr;
+    ListNode* cur = head;
+    ListNode* tmp;
+    while (cur) {
+      tmp = cur->next;
+      cur->next = pre;
+
+      // 更新 pre cur
+      pre = cur;
+      cur = tmp;
+    }
+    return pre;
+  }
+  ListNode* reverse(ListNode* pre, ListNode* cur) {
+    if (cur == nullptr) return pre;
+    ListNode* tmp = cur->next;
+    cur->next = pre;
+
+    return reverse(cur, tmp);
+  }
+  ListNode* reverseList2(ListNode* head) { return reverse(nullptr, head); }
+
+  /* 24. 两两交换链表中的节点 */
+  ListNode* swapPairs(ListNode* head) {
+    ListNode* dummy = new ListNode(0, head);
+    ListNode* cur = dummy;
+
+    while (cur->next && cur->next->next) {
+      ListNode* tmp1 = cur->next;
+      ListNode* tmp2 = cur->next->next;
+      tmp1->next = tmp2->next;
+      tmp2->next = tmp1;
+      cur->next = tmp2;
+    }
+    return dummy->next;
+  }
+
+  void DeleteAtIndex(ListNode* head, int index) {
+    ListNode* dummy = new ListNode(0, head);
+    ListNode* cur = dummy;
+
+    while (index-- && cur->next != nullptr) {
+      cur = cur->next;
+    }
+    ListNode* tmp = cur->next;
+    cur->next = tmp->next;
+    delete tmp;
+  }
+  ListNode* removeNthFromEnd(ListNode* head, int n) {
+    int size = 0;
+    ListNode* cur = head;
+    while (cur) {
+      size++;
+      cur = cur->next;
+    }
+    DeleteAtIndex(head, size - n);
+    return head;
+  }
 };
 
+// 注意什么时候取 dummy->next 或 dummy
 class MyLinkedList {
  public:
-  MyLinkedList() : head(nullptr) {}
+  MyLinkedList() {
+    dummy_head_ = new ListNode(0);
+    size_ = 0;
+  }
 
+  // todo:...
   int get(int index) {
-    ListNode* cur = head;
-    while (cur && index > 0) {
-      cur = cur->next;
-      index--;
-    }
-    if (cur == nullptr || index < 0)
+    if (index > size_ - 1 || index < 0) {
       return -1;
-    else
-      return cur->val;
+    }
+    ListNode* cur = dummy_head_->next;
+    while (index--) {
+      cur = cur->next;
+    }
+    return cur->val;
   }
 
   void addAtHead(int val) {
-    ListNode* new_head = new ListNode(val);
-    if (head) {
-      new_head->next = head;
-    }
-    head = new_head;
+    ListNode* node = new ListNode(val, dummy_head_->next);
+    dummy_head_->next = node;
+    size_++;
   }
 
   void addAtTail(int val) {
-    ListNode* cur = head;
-    while (cur && cur->next) {
+    ListNode* cur = dummy_head_;
+    while (cur->next != nullptr) {
       cur = cur->next;
     }
-    cur->next = new ListNode(val);
+    ListNode* node = new ListNode(val);
+    cur->next = node;
+    size_++;
   }
 
   void addAtIndex(int index, int val) {
-    ListNode* cur = head;
-    while (cur && index > 1) {
-      cur = cur->next;
-      index--;
+    if (index > size_ || index < 0) {
+      return;
     }
-    ListNode* new_node = new ListNode(val, cur->next);
-    cur->next = new_node;
+    ListNode* cur = dummy_head_;
+    while (index--) {
+      cur = cur->next;
+    }
+    ListNode* node = new ListNode(val, cur->next);
+    cur->next = node;
+    size_++;
   }
 
   void deleteAtIndex(int index) {
-    ListNode* cur = head;
-    while (cur && index > 1) {
+    if (index >= size_ || index < 0) {
+      return;
+    }
+    ListNode* cur = dummy_head_;
+    while (index--) {
       cur = cur->next;
-      index--;
     }
     ListNode* tmp = cur->next;
     cur->next = cur->next->next;
     delete tmp;
+    size_--;
   }
 
  private:
-  ListNode* head;
+  ListNode* dummy_head_;
+  int size_;
 };
 
 int main() {
